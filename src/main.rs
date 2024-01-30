ba3a80069a392ed8fe2759b0f443351f40aec424
CodeSandbox <support@codesandbox.io>
2024-01-30 14:15:29 +0000

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("1e4 is {}, -2.5e-3 is {}", 1e4, -2.5e-3);
+    let mut idade_em_dias = String::new();
+    io::stdin()
+        .read_line(&mut idade_em_dias)
+        .expect("Idade não informada");
+
+    let idade_em_dias: u32 = idade_em_dias.trim().parse().expect("Idade inválida");
+
+    let anos = idade_em_dias / 365;
+    let meses = (idade_em_dias % 365) / 30;
+    let dias = (idade_em_dias % 365) % 30;
+
+    println!("{} ano(s)", anos);
+    println!("{} mes(es)", meses);
+    println!("{} dia(s)", dias);
 }
