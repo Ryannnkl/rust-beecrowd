c81f91a155c806cc7e72bdf7091fb073e7831723
Ryann Ferreira <ryannnkl@gmail.com>
2024-01-30 15:22:22 +0000

    1010 - Cálculo Simples

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 use std::io;
 
-fn main() {
-    let mut idade_em_dias = String::new();
+fn read_pedido() -> (f64, f64) {
+    let mut input = String::new();
+
     io::stdin()
-        .read_line(&mut idade_em_dias)
-        .expect("Idade não informada");
+        .read_line(&mut input)
+        .expect("Erro ao ler a entrada");
 
-    let idade_em_dias: u32 = idade_em_dias.trim().parse().expect("Idade inválida");
+    let mut iter = input.trim().split_whitespace();
+    iter.next(); // Ignorar o primeiro elemento
 
-    let anos = idade_em_dias / 365;
-    let meses = (idade_em_dias % 365) / 30;
-    let dias = (idade_em_dias % 365) % 30;
+    let quantidade = iter
+        .next()
+        .expect("Quantidade não fornecida")
+        .parse::<f64>()
+        .expect("Erro ao converter quantidade");
+    let valor = iter
+        .next()
+        .expect("Valor não fornecido")
+        .parse::<f64>()
+        .expect("Erro ao converter valor");
 
-    println!("{} ano(s)", anos);
-    println!("{} mes(es)", meses);
-    println!("{} dia(s)", dias);
+    (quantidade, valor)
+}
+
+fn main() {
+    let (pedido1_quantidade, pedido1_valor) = read_pedido();
+    let (pedido2_quantidade, pedido2_valor) = read_pedido();
+
+    let total = (pedido1_quantidade * pedido1_valor) + (pedido2_quantidade * pedido2_valor);
+
+    println!("VALOR A PAGAR: R$ {:.2}", total);
 }
