a44b73ebe39e17ee09068b1e3d29d8a872eea26c
Ryann Ferreira <ryannnkl@gmail.com>
2024-01-31 18:49:50 +0000

    1013 - O Maior

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,19 @@
 use std::io;
 
-fn read_pedido() -> (f64, f64) {
-    let mut input = String::new();
+fn main() {
+    let mut numbers = String::new();
 
     io::stdin()
-        .read_line(&mut input)
-        .expect("Erro ao ler a entrada");
+        .read_line(&mut numbers)
+        .expect("Failed to read line");
 
-    let mut iter = input.trim().split_whitespace();
-    iter.next(); // Ignorar o primeiro elemento
+    let mut numbers = numbers.trim().split_whitespace();
+    let a = numbers.next().unwrap().parse::<i32>().unwrap();
+    let b = numbers.next().unwrap().parse::<i32>().unwrap();
+    let c = numbers.next().unwrap().parse::<i32>().unwrap();
 
-    let quantidade = iter
-        .next()
-        .expect("Quantidade não fornecida")
-        .parse::<f64>()
-        .expect("Erro ao converter quantidade");
-    let valor = iter
-        .next()
-        .expect("Valor não fornecido")
-        .parse::<f64>()
-        .expect("Erro ao converter valor");
+    let maior_ab = (a + b + (a - b).abs()) / 2;
+    let maior_abc = (maior_ab + c + (maior_ab - c).abs()) / 2;
 
-    (quantidade, valor)
-}
-
-fn main() {
-    let (pedido1_quantidade, pedido1_valor) = read_pedido();
-    let (pedido2_quantidade, pedido2_valor) = read_pedido();
-
-    let total = (pedido1_quantidade * pedido1_valor) + (pedido2_quantidade * pedido2_valor);
-
-    println!("VALOR A PAGAR: R$ {:.2}", total);
+    println!("{} eh o maior", maior_abc);
 }
